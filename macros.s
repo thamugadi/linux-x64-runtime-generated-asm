26301f8ed7a8d60e9f082a7804d32b4f66018d9f
aramya <mdnh@protonmail.com>
2022-10-10 19:03:59 +0100

    Update macros.s

--- macros.s
+++ macros.s
@@ -70,12 +70,3 @@ inc rbx
 mov byte ptr [rbx], 0x05
 inc rbx
 .endm
-
-##//example:
-.section .text
-.exec_perm 0x402000 0x100
-.make_syscall 0x402000 60 0 0 0 0 0 0
-jmp 0x402000
-.section .data
-beta:
-.asciz "abcdefghijklmnopqrstuvwxyz"
